a29c0ab56606adb49b5a6feec4a1ae03ceb7ef8f
Diego Carmona Bernal <carmonabernaldiego@gmail.com>
2021-03-24 23:27:24 -0600

    update

--- user/form_default.php
+++ user/form_default.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin.php');
+require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 
 $_SESSION['user_id'] = array();
 $_SESSION['email'] = array();
