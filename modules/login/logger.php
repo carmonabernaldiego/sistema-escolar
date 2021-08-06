ac176389836e9f12cbed6a3424a4a53de6890bad
Diego Carmona Bernal <carmonabernaldiego@gmail.com>
2021-08-06 00:11:44 -0500

    update

--- modules/login/logger.php
+++ modules/login/logger.php
@@ -102,9 +102,12 @@ if (!empty($_POST['txtuser']) and !empty($_POST['txtpass'])) {
 					<div class="forgot-pass">
                         <a class="un" href="#">¿Olvidaste la contraseña?</a>
                     </div>
-                    <div class="pretty p-default p-round p-smooth">
+                    <div class="pretty p-svg p-round p-curve p-smooth">
                         <input type="checkbox" name="remember_session" placeholder="Recordar" value="1" />
                         <div class="state p-primary">
+                            <svg class="svg svg-icon" viewBox="0 0 20 20">
+                                <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                            </svg>
                             <label class="remember">Recuérdame</label>
                         </div>
                     </div>
@@ -120,9 +123,12 @@ if (!empty($_POST['txtuser']) and !empty($_POST['txtpass'])) {
             <div class="forgot-pass">
                 <a class="un" href="#">¿Olvidaste la contraseña?</a>
             </div>           
-            <div class="pretty p-default p-round p-smooth">
+            <div class="pretty p-svg p-round p-curve p-smooth">
                 <input type="checkbox" name="remember_session" placeholder="Recordar" value="1" />
                 <div class="state p-primary">
+                    <svg class="svg svg-icon" viewBox="0 0 20 20">
+                        <path d="M7.629,14.566c0.125,0.125,0.291,0.188,0.456,0.188c0.164,0,0.329-0.062,0.456-0.188l8.219-8.221c0.252-0.252,0.252-0.659,0-0.911c-0.252-0.252-0.659-0.252-0.911,0l-7.764,7.763L4.152,9.267c-0.252-0.251-0.66-0.251-0.911,0c-0.252,0.252-0.252,0.66,0,0.911L7.629,14.566z" style="stroke: white;fill:white;"></path>
+                    </svg>
                     <label class="remember">Recuérdame</label>
                 </div>
             </div>
