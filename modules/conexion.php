0a79572176a56c4844ddfe564b32b48c71ce7e92
Diego Carmona Bernal <carmonabernaldiego@gmail.com>
2021-08-24 15:30:00 -0500

    update

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,11 +1,11 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
-mysqli_set_charset($conexion, 'utf8');
 
 $conexion = mysqli_connect("localhost", "root", "", "db_escolar");
 
-/* comprobar la conexión */
 if (mysqli_connect_errno()) {
 	printf("Falló la conexión: %s\n", mysqli_connect_error());
 	exit();
 }
+
+mysqli_set_charset($conexion, 'utf8');
