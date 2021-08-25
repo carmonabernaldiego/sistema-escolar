2943463f44ad27f356d36f88c43e9132fcbc5ca9
carmonabernaldiego <carmonabernaldiego@gmail.com>
2021-08-25 15:30:23 -0500

    update

--- js/schoolperiods.js
+++ js/schoolperiods.js
@@ -30,3 +30,16 @@ $("#datespend").focus(function() {
     dateEnd.show();
     dateStart.hide();
 });
+
+let value;
+
+$('#selectactive').on('select2:select', function(e) {
+    value = e.params.data.id;
+
+    if (value == 0) {
+        $('#selectcurrent').prop('disabled', true);
+        $('#selectcurrent').val('0').trigger('change.select2');
+    } else if (value == 1) {
+        $('#selectcurrent').prop('disabled', false);
+    }
+});
