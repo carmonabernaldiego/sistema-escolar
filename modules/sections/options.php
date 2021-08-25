fd7104f813626d019d1861f3616621c47011c340
carmonabernaldiego <carmonabernaldiego@gmail.com>
2021-08-25 12:38:56 -0500

    update

--- modules/sections/options.php
+++ modules/sections/options.php
@@ -4,17 +4,17 @@ include_once 'security.php';
 <div class="form-options">
     <div class="options">
         <form action="" method="POST">
-            <button id="btnAddOptions" class="btn btnadd icon" name="btn" value="form_add" type="submit">add</button>
+            <button id="btnAddOptions" class="btn btn-add icon" name="btn" value="form_add" type="submit">add</button>
         </form>
         <form action="" method="POST">
-            <button class="btn disabled icon" name="btn" value="form_coding" type="submit" disabled>code</button>
+            <button class="btn btn-disabled icon" name="btn" value="form_coding" type="submit" disabled>code</button>
         </form>
         <form action="" method="POST">
-            <button class="btn disabled icon" name="btn" value="form_printer" type="submit" disabled>print</button>
+            <button class="btn btn-disabled icon" name="btn" value="form_printer" type="submit" disabled>print</button>
         </form>
-        <button id="btnSearchMobile" class="btn search-mobile btnsearch icon">search</button>
+        <button id="btnSearchMobile" class="btn btn-search-mobile icon">search</button>
         <form action="/">
-            <button id="btnExitOptions" class="btn btnexit icon" type="submit">exit_to_app</button>
+            <button id="btnExitOptions" class="btn btn-exit icon" type="submit">exit_to_app</button>
         </form>
     </div>
     <div class="search">
