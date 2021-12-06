96b3b059c7bee7af85e59530b70783953418abf1
Diego Carmona Bernal <cbdiegox@gmail.com>
2021-12-05 21:19:57 -0600

    update

---
+++ modules/sections/404.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página no encontrada</title>
+    <!-- core:css -->
+    <link rel="stylesheet" href="/uiadmin/assets/vendors/core/core.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- end plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="/uiadmin/assets/fonts/feather-font/css/iconfont.css">
+    <link rel="stylesheet" href="/uiadmin/assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="/uiadmin/assets/css/demo_1/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="/images/icon.png" />
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="page-wrapper full-page">
+            <div class="page-content d-flex align-items-center justify-content-center">
+
+                <div class="row w-100 mx-0 auth-page">
+                    <div class="col-md-8 col-xl-6 mx-auto d-flex flex-column align-items-center">
+                        <img src="/images/404.svg" class="img-fluid mb-2" alt="404">
+                        <h1 class="font-weight-bold mb-22 mt-2 tx-80 text-muted">404</h1>
+                        <h4 class="mb-2">Página No Encontrada</h4>
+                        <h6 class="text-muted mb-3 text-center">Oopps !! La página que buscaba no existe.</h6>
+                        <a href="/" class="btn btn-primary">Regresar a inicio</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- core:js -->
+    <script src="/uiadmin/assets/vendors/core/core.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <!-- end plugin js for this page -->
+    <!-- inject:js -->
+    <script src="/uiadmin/assets/vendors/feather-icons/feather.min.js"></script>
+    <script src="/uiadmin/assets/js/template.js"></script>
+    <!-- endinject -->
+    <!-- custom js for this page -->
+    <!-- end custom js for this page -->
+</body>
+
+</html>
