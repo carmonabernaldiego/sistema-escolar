8df239f0508ce95579ca617a2a10be55dca2b64b
CBDX <cbdiegox@gmail.com>
2022-02-03 02:06:31 -0600

    update

--- modules/administratives/form_add.php
+++ modules/administratives/form_add.php
@@ -20,9 +20,9 @@ $id_generate = 'admin' . unique_id(5);
                     <input id="txtuserid" class="text" style=" display: none;" type="text" name="txtuserid" value="<?php echo $id_generate; ?>" maxlength="50" required />
                     <input class="text" type="text" name="txt" value="<?php echo $id_generate; ?>" required disabled />
                     <label for="txtusername" class="label">Nombre</label>
-                    <input id="txtusername" class="text" type="text" name="txtname" value="" maxlength="30" required autofocus />
+                    <input id="txtusername" class="text" type="text" name="txtname" value="" placeholder="Nombre" maxlength="30" required autofocus />
                     <label for="txtusersurnames" class="label">Apellidos</label>
-                    <input id="txtusersurnames" class="text" type="text" name="txtsurnames" value="" maxlength="60" required />
+                    <input id="txtusersurnames" class="text" type="text" name="txtsurnames" placeholder="Apellidos" value="" maxlength="60" required />
                     <label for="selectgender" class="label">Género</label>
                     <select id="selectgender" class="select" name="selectgender" required>
                         <option value="">Seleccioné</option>
@@ -32,17 +32,17 @@ $id_generate = 'admin' . unique_id(5);
                         <option value="nodecirlo">Prefiero no decirlo</option>
                     </select>
                     <label for="dateofbirth" class="label">Fecha de nacimiento</label>
-                    <input id="dateofbirth" class="date" type="text" name="dateofbirth" value="" pattern="\d{4}-\d{2}-\d{2}" placeholder="YYYY-MM-DD" maxlength="10" required />
+                    <input id="dateofbirth" class="date" type="text" name="dateofbirth" value="" pattern="\d{4}-\d{2}-\d{2}" placeholder="aaaa-mm-dd" maxlength="10" required />
                     <label for="txtusercurp" class="label">CURP</label>
-                    <input id="txtusercurp" class="text" type="text" name="txtcurp" value="" maxlength="18" onkeyup="this.value = this.value.toUpperCase()" required />
+                    <input id="txtusercurp" class="text" type="text" name="txtcurp" value="" placeholder="Clave Única de Registro de Población" maxlength="18" onkeyup="this.value = this.value.toUpperCase()" required />
                 </div>
                 <div class="last">
                     <label for="txtuserrfc" class="label">RFC</label>
-                    <input id="txtuserrfc" class="text" type="text" name="txtrfc" value="" maxlength="13" onkeyup="this.value = this.value.toUpperCase()" required />
+                    <input id="txtuserrfc" class="text" type="text" name="txtrfc" value="" placeholder="XAXX010101000" maxlength="13" onkeyup="this.value = this.value.toUpperCase()" required />
                     <label for="txtuserphone" class="label">Telefono</label>
                     <input id="txtuserphone" class="text" type="text" name="txtphone" value="" pattern="[0-9]{10}" title="Ingresa un número de teléfono válido." placeholder="9998887766" maxlength="10" required />
                     <label for="txtuseraddress" class="label">Domicilio</label>
-                    <input id="txtuseraddress" class="text" type="text" name="txtaddress" value="" maxlength="100" required />
+                    <input id="txtuseraddress" class="text" type="text" name="txtaddress" value="" placeholder="Domicilio" maxlength="100" required />
                     <label for="selectlevelstudies" class="label">Nivel de estudios</label>
                     <select id="selectlevelstudies" class="select" name="selectlevelstudies" required>
                         <option value="">Seleccioné</option>
@@ -52,9 +52,9 @@ $id_generate = 'admin' . unique_id(5);
                         <option value="Doctorado">Doctorado</option>
                     </select>
                     <label for="txtuseroccupation" class="label">Cargo</label>
-                    <input id="txtuseroccupation" class="text" type="text" name="txtoccupation" value="" maxlength="100" required />
+                    <input id="txtuseroccupation" class="text" type="text" name="txtoccupation" value="" placeholder="Cargo" maxlength="100" required />
                     <label for="txtuserobservation" class="label">Observación</label>
-                    <input id="txtuserobservation" class="text" type="text" name="txtobservation" value="" maxlength="200" />
+                    <input id="txtuserobservation" class="text" type="text" name="txtobservation" value="" placeholder="Observación" maxlength="200" />
                 </div>
             </div>
             <button id="btnBack" class="btn back icon" type="button">arrow_back</button>
