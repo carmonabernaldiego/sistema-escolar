4f7447da429a71248309d9f3c2f4b4cd4aea824c
CBDX <cbdiegox@gmail.com>
2022-02-08 14:11:20 -0600

    update

--- modules/teachers/load_data.php
+++ modules/teachers/load_data.php
@@ -1,57 +1,71 @@
 <?php
+include_once '../notif_info_msgbox.php';
+
 require_once($_SESSION['raiz'] . '/modules/sections/role-access-admin-editor.php');
 
-$sql = "SELECT COUNT(user) AS total FROM teachers";
+$sql = "SELECT COUNT(career) AS total FROM careers";
 
 if ($result = $conexion->query($sql)) {
 	if ($row = mysqli_fetch_array($result)) {
-		$tpages = ceil($row['total'] / $max);
-	}
-}
+		if ($row['total'] == 0) {
+			Error('Crea como mínimo una carrera para agregar docentes.');
+			header('Location: /modules/careers');
+			exit();
+		} else {
+			$sql = "SELECT COUNT(user) AS total FROM teachers";
 
-if (!empty($_POST['search'])) {
-	$_POST['search'] = trim($_POST['search']);
-	$_POST['search'] = mysqli_real_escape_string($conexion, $_POST['search']);
+			if ($result = $conexion->query($sql)) {
+				if ($row = mysqli_fetch_array($result)) {
+					$tpages = ceil($row['total'] / $max);
+				}
+			}
 
-	$_SESSION['user_id'] = array();
-	$_SESSION['teacher_name'] = array();
-	$_SESSION['teacher_curp'] = array();
-	$_SESSION['teacher_phone'] = array();
+			if (!empty($_POST['search'])) {
+				$_POST['search'] = trim($_POST['search']);
+				$_POST['search'] = mysqli_real_escape_string($conexion, $_POST['search']);
 
-	$i = 0;
+				$_SESSION['user_id'] = array();
+				$_SESSION['teacher_name'] = array();
+				$_SESSION['teacher_curp'] = array();
+				$_SESSION['teacher_phone'] = array();
 
-	$sql = "SELECT * FROM teachers WHERE user LIKE '%" . $_POST['search'] . "%' OR name LIKE '%" . $_POST['search'] . "%' OR surnames LIKE '%" . $_POST['search'] . "%' OR curp LIKE '%" . $_POST['search'] . "%' OR phone LIKE '%" . $_POST['search'] . "%' ORDER BY name";
+				$i = 0;
 
-	if ($result = $conexion->query($sql)) {
-		while ($row = mysqli_fetch_array($result)) {
-			$_SESSION['user_id'][$i] = $row['user'];
-			$_SESSION['teacher_curp'][$i] = $row['curp'];
-			$_SESSION['teacher_name'][$i] = $row['name'] . ' ' . $row['surnames'];
-			$_SESSION['teacher_phone'][$i] = $row['phone'];
+				$sql = "SELECT * FROM teachers WHERE user LIKE '%" . $_POST['search'] . "%' OR name LIKE '%" . $_POST['search'] . "%' OR surnames LIKE '%" . $_POST['search'] . "%' OR curp LIKE '%" . $_POST['search'] . "%' OR phone LIKE '%" . $_POST['search'] . "%' ORDER BY name";
 
-			$i += 1;
+				if ($result = $conexion->query($sql)) {
+					while ($row = mysqli_fetch_array($result)) {
+						$_SESSION['user_id'][$i] = $row['user'];
+						$_SESSION['teacher_curp'][$i] = $row['curp'];
+						$_SESSION['teacher_name'][$i] = $row['name'] . ' ' . $row['surnames'];
+						$_SESSION['teacher_phone'][$i] = $row['phone'];
+
+						$i += 1;
+					}
+				}
+				$_SESSION['total_users'] = count($_SESSION['user_id']);
+			} else {
+				$_SESSION['user_id'] = array();
+				$_SESSION['teacher_name'] = array();
+				$_SESSION['teacher_curp'] = array();
+				$_SESSION['teacher_phone'] = array();
+
+				$i = 0;
+
+				$sql = "SELECT * FROM teachers ORDER BY created_at DESC, user, name LIMIT $inicio, $max";
+
+				if ($result = $conexion->query($sql)) {
+					while ($row = mysqli_fetch_array($result)) {
+						$_SESSION['user_id'][$i] = $row['user'];
+						$_SESSION['teacher_curp'][$i] = $row['curp'];
+						$_SESSION['teacher_name'][$i] = $row['name'] . ' ' . $row['surnames'];
+						$_SESSION['teacher_phone'][$i] = $row['phone'];
+
+						$i += 1;
+					}
+				}
+				$_SESSION['total_users'] = count($_SESSION['user_id']);
+			}
 		}
 	}
-	$_SESSION['total_users'] = count($_SESSION['user_id']);
-} else {
-	$_SESSION['user_id'] = array();
-	$_SESSION['teacher_name'] = array();
-	$_SESSION['teacher_curp'] = array();
-	$_SESSION['teacher_phone'] = array();
-
-	$i = 0;
-
-	$sql = "SELECT * FROM teachers ORDER BY created_at DESC, user, name LIMIT $inicio, $max";
-
-	if ($result = $conexion->query($sql)) {
-		while ($row = mysqli_fetch_array($result)) {
-			$_SESSION['user_id'][$i] = $row['user'];
-			$_SESSION['teacher_curp'][$i] = $row['curp'];
-			$_SESSION['teacher_name'][$i] = $row['name'] . ' ' . $row['surnames'];
-			$_SESSION['teacher_phone'][$i] = $row['phone'];
-
-			$i += 1;
-		}
-	}
-	$_SESSION['total_users'] = count($_SESSION['user_id']);
 }
