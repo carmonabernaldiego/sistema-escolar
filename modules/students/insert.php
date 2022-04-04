bc7ce7e5fca97f94a4acc30371196b485c946e71
CBDX <cbdiegox@gmail.com>
2022-04-03 20:00:58 -0500

    update

--- modules/students/insert.php
+++ modules/students/insert.php
@@ -13,34 +13,27 @@ if (empty($_POST['txtuserid'])) {
 }
 if ($_POST['txtuserid'] == '') {
 	Error('Ingrese un ID correcto.');
-	header('Location: /modules/teachers');
+	header('Location: /modules/students');
 	exit();
 }
 
-$sql = "SELECT * FROM teachers WHERE user = '" . $_POST['txtuserid'] . "'";
+$sql = "SELECT * FROM students WHERE user = '" . $_POST['txtuserid'] . "'";
 
 if ($result = $conexion->query($sql)) {
 	if ($row = mysqli_fetch_array($result)) {
 		Error('Este ID ya está en uso. Elige otro.');
-		header('Location: /modules/teachers');
+		header('Location: /modules/students');
 		exit();
 	} else {
 		$date = date('Y-m-d H:i:s');
-		$careers = '';
 
-		for ($i = 0; $i < count($_POST["selectCareers"]); $i++) {
-			$careers .= $_POST["selectCareers"][$i] . ',';
-		}
-
-		$careers = trim($careers, ',');
-
-		$sql_insert_user = "INSERT INTO users(user, pass, permissions, image, created_at) VALUES('" . trim($_POST['txtuserid']) . "', '" . trim($_POST['txtuserid']) . "', 'teacher', 'user.png','" . $date . "')";
+		$sql_insert_user = "INSERT INTO users(user, pass, permissions, image, created_at) VALUES('" . trim($_POST['txtuserid']) . "', '" . trim($_POST['txtuserid']) . "', 'student', 'user.png','" . $date . "')";
 
 		if (mysqli_query($conexion, $sql_insert_user)) {
-			$sql_insert_teacher = "INSERT INTO teachers(user, name, surnames, curp, rfc, gender, date_of_birth, phone, address, level_studies, specialty, career, created_at) VALUES('" . trim($_POST['txtuserid']) . "', '" . trim($_POST['txtname']) . "', '" . trim($_POST['txtsurnames']) . "', '" . trim($_POST['txtcurp']) . "', '" . trim($_POST['txtrfc']) . "', '" . trim($_POST['selectgender']) . "', '" . trim($_POST['dateofbirth']) . "', '" . trim($_POST['txtphone']) . "', '" . trim($_POST['txtaddress']) . "', '" . trim($_POST['selectlevelstudies']) . "', '" . trim($_POST['txtspecialty']) . "', '" . $careers . "', '" . $date . "')";
+			$sql_insert_teacher = "INSERT INTO students(user, name, surnames, curp, rfc, gender, date_of_birth, phone, address, career, documentation, admission_date, created_at) VALUES('" . trim($_POST['txtuserid']) . "', '" . trim($_POST['txtname']) . "', '" . trim($_POST['txtsurnames']) . "', '" . trim($_POST['txtcurp']) . "', '" . trim($_POST['txtrfc']) . "', '" . trim($_POST['selectGender']) . "', '" . trim($_POST['dateofbirth']) . "', '" . trim($_POST['txtphone']) . "', '" . trim($_POST['txtaddress']) . "', '" . trim($_POST['selectCareer']) . "', '" . trim($_POST['selectDocumentation']) . "', '" . trim($_POST['dateadmission']) . "', '" . $date . "')";
 
 			if (mysqli_query($conexion, $sql_insert_teacher)) {
-				Info('Personal docente agregado.');
+				Info('Alumno agregado.');
 			} else {
 				$sql_delete_users = "DELETE FROM users WHERE user = '" . trim($_POST['txtuserid']) . "'";
 
@@ -51,7 +44,7 @@ if ($result = $conexion->query($sql)) {
 		} else {
 			Error('Error al guardar.');
 		}
-		header('Location: /modules/teachers');
+		header('Location: /modules/students');
 		exit();
 	}
 }
